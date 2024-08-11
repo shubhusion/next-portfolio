28147bbf507e9b5c1a0b4b37bac0ba4d9df69075
shubhusion <shubham27.sharma03@gmail.com>
2024-08-11 12:33:20 +0530

    rectified errors and warnings

--- components/skills/SkillCard.tsx
+++ components/skills/SkillCard.tsx
@@ -1,33 +1,49 @@
-import Image from "next/image"
-import { useEffect, useState } from 'react'
+import Image from "next/image";
+import { useEffect, useState } from 'react';
 import { FastAverageColor } from 'fast-average-color';
 import { skill } from "@/types/main";
 import { useTheme } from "next-themes";
 
 const Skill = ({ name, image }: skill) => {
-
     const { theme } = useTheme();
-    const [bgColor, setBgColor] = useState("")
+    const [bgColor, setBgColor] = useState('rgba(0, 0, 0, 0.07)'); // Default background color
+    const [isClient, setIsClient] = useState(false);
+
     useEffect(() => {
+        setIsClient(true); // Indicate that we are on the client-side
+
         new FastAverageColor().getColorAsync(image)
             .then(color => {
-                const rgba = color.rgb.split(')')
-                setBgColor(rgba[0] + ',0.07)')
+                const rgba = color.rgb.split(')');
+                setBgColor(rgba[0] + ',0.07)');
             })
             .catch(e => {
-                console.log(e);
-            })
-    }, [image])
+                console.error(e);
+            });
+    }, [image]);
+
+    if (!isClient) {
+        return <div className="h-20 w-20 md:h-24 md:w-24 rounded-full bg-gray-100 dark:bg-grey-800" />;
+    }
 
     return (
         <div className="flex flex-col justify-center items-center gap-2">
-            <div title={name} style={{ backgroundColor: bgColor }}
-                className={"h-20 w-20 md:h-24 md:w-24 rounded-full bg-gray-100 dark:bg-grey-800 flex items-center justify-center"}>
-                <Image alt="skill" width={100} height={100} className={`h-12 w-12 md:h-14 md:w-14 object-contain ${theme === 'dark' && (name === "GitHub" || name === "Vercel" || name === "NextJS" || name === "ExpressJS" ? 'invert' : 'invert-0')}`} src={image} />
+            <div
+                title={name}
+                style={{ backgroundColor: bgColor }}
+                className="h-20 w-20 md:h-24 md:w-24 rounded-full bg-gray-100 dark:bg-grey-800 flex items-center justify-center"
+            >
+                <Image
+                    alt={name}
+                    width={100}
+                    height={100}
+                    className={`h-12 w-12 md:h-14 md:w-14 object-contain ${theme === 'dark' && (name === "GitHub" || name === "Vercel" || name === "NextJS" || name === "ExpressJS" ? 'invert' : 'invert-0')}`}
+                    src={image}
+                />
             </div>
             <p className="text-sm md:text-base">{name}</p>
         </div>
-    )
+    );
 }
 
-export default Skill
+export default Skill;
